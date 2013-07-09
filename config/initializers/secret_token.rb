201b456c01f7a6dfabdbf7faa55c7588ffa4558f
themollusk45 <themollusk45@gmail.com>
2013-07-09 12:29:39 -0400

    users and static pages made

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageGalleryApp::Application.config.secret_token = '6bfb5c0fdd63cce69769f0e890e9ff59790a0344e98a055fe0d81b6b7ec8b24552575aafaf5d25638cfc72e5088baf60007c5708d8d4df9f486bd03ab6a5ac22'
